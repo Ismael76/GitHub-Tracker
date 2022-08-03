834b94d301c138cd67d3c37f50c05491e9e76d58
Ismael Ali <ismaelali78@outlook.com>
2022-08-03 18:04:20 +0100

    added footer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,19 @@ function App() {
   return (
     <div className="App">
       <SearchForm />
+      <footer>
+        <p>Created By Muhammed Ismael & Reece</p>
+        <p className="footerContainer">
+          <a href="https://github.com/Ismael76">
+            <i class="fa fa-github-square github icons" id="github"></i>
+            <p>Ismael</p>
+          </a>
+          <a href="https://github.com/reececilon">
+            <i class="fa fa-github-square github icons" id="github"></i>
+            <p>Reece</p>
+          </a>
+        </p>
+      </footer>
     </div>
   );
 }
